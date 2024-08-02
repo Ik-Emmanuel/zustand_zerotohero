78320ab76d80de19a7a03f85e4f79b082d4b7317
Ik-Emmanuel <nwokochaikechukwu13@gmail.com>
2024-08-02 14:06:42 +0100

    using the store in application

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { useStore } from "@/store/store";
 import { create } from "zustand";
 
 export default function Home() {
+  const store = useStore();
   return (
     <main>
       <div className="h-full w-full mx-auto my-auto flex flex-col gap-4 justify-center items-center">
-        <p> Hi</p>
+        <p> {JSON.stringify(store)}</p>
       </div>
     </main>
   );
