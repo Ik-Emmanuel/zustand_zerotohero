51876a1ba0366e4857924adf102594e255d5d4fc
Ik-Emmanuel <nwokochaikechukwu13@gmail.com>
2024-08-02 14:18:39 +0100

    best ways to pick item from store

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,34 @@
 import { Button } from "@/components/ui/button";
 import { useStore } from "@/store/store";
 import { create } from "zustand";
+import { useShallow } from "zustand/react/shallow";
 
 export default function Home() {
   // different way to use store
   // 1. get the whole store
   const store = useStore();
 
-  // 2. destructuring what you need form the store
-  const { age, fullName, userName, address } = useStore((state) => ({
-    age: state.age,
-    fullName: state.fullName,
-    userName: state.userName,
-    address: state.address,
-  }));
+  // // 2. destructuring what you need form the store
+  // const { age, fullName, userName, address } = useStore((state) => ({
+  //   age: state.age,
+  //   fullName: state.fullName,
+  //   userName: state.userName,
+  //   address: state.address,
+  // }));
+
+  // 3. most optimised version
+  //  allows you quickly get what you need from the store
+  const { age, fullName, userName, address } = useStore(
+    useShallow((state) => ({
+      age: state.age,
+      fullName: state.fullName,
+      userName: state.userName,
+      address: state.address,
+    }))
+  );
+
+  // another way to pick what is needed from the store causing less component rerendering (for just single item selection from store)
+  const userAddress = useStore((state) => state.address);
 
   return (
     <main>
